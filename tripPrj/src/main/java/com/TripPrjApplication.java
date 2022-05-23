af052009a9fd87d8ef583eadf88bdb1a98c2e121
Jin <jinhyo.dev@gmail.com>
2022-05-23 12:19:49 +0900

    Initial commit

---
+++ tripPrj/src/main/java/com/TripPrjApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPrjApplication.class, args);
+	}
+
+}
